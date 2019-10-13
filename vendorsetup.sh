7425782d6257aeb9717c39c0b18cab8e822d20d3
Mauro Rossi <issor.oruam@gmail.com>
2019-10-12 21:26:36 -0400

    [bringup] Initial start on Q
    Changes to vendor and patches provided by @maurossi & @goffioul.

---
+++ vendorsetup.sh
@@ -0,0 +1,70 @@
+
+# autopatch.sh: script to manage patches on top of repo
+# Copyright (C) 2019 Intel Corporation. All rights reserved.
+# Author: sgnanase <sundar.gnanasekaran@intel.com>
+# Author: Sun, Yi J <yi.j.sun@intel.com>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+# save the official lunch command to aosp_lunch() and source it
+tmp_lunch=`mktemp`
+sed '/ lunch()/,/^}/!d'  build/envsetup.sh | sed 's/function lunch/function aosp_lunch/' > ${tmp_lunch}
+source ${tmp_lunch}
+rm -f ${tmp_lunch}
+
+function  apply_patch
+{
+
+local patch_folder=$1
+echo "patch folder: $patch_folder"
+
+}
+
+# Override lunch function to filter lunch targets
+function lunch
+{
+    local T=$(gettop)
+    if [ ! "$T" ]; then
+        echo "[lunch] Couldn't locate the top of the tree.  Try setting TOP." >&2
+        return
+    fi
+
+    aosp_lunch $*
+
+    rm -rf vendor/x86/Android.mk
+    vendor/x86/autopatch.sh
+
+}
+
+# Get the exact value of a build variable.
+function get_build_var()
+{
+    if [ "$1" = "COMMON_LUNCH_CHOICES" ]
+    then
+        valid_targets=`mixinup -t`
+        save=`build/soong/soong_ui.bash --dumpvar-mode $1`
+        unset LUNCH_MENU_CHOICES
+        for t in ${save[@]}; do
+            array=(${t/-/ })
+            target=${array[0]}
+            if [[ "${valid_targets}" =~ "$target" ]]; then
+                   LUNCH_MENU_CHOICES+=($t)
+            fi
+        done
+        echo ${LUNCH_MENU_CHOICES[@]}
+        return
+    else
+        if [ "$BUILD_VAR_CACHE_READY" = "true" ]
+        then
+            eval "echo \"\${var_cache_$1}\""
+            return
+        fi
+
+        local T=$(gettop)
+        if [ ! "$T" ]; then
+            echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
+            return
+        fi
+        (\cd $T; build/soong/soong_ui.bash --dumpvar-mode $1)
+    fi
+}
